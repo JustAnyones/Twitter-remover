17f92190e9b3b9b3c733f27434ebc2ffd83dcc60
JustAnyone <dom.svetikas@gmail.com>
2021-07-30 16:35:47 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         No twitter for you
+// @namespace    https://github.com/JustAnyones/Twitter-remover/
+// @version      0.1
+// @description  Prevents you from opening twitter at any cost.
+// @author       JustAnyone
+// @match        https://twitter.com/*
+// @match        https://mobile.twitter.com/*
+// @icon         https://www.google.com/s2/favicons?domain=twitter.com/
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.stop();
+    window.location.replace("https://www.bible.com/en-GB/bible/59/GEN.1.ESV"); // lol
+})();
