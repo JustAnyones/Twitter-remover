4a8b2f958abc731280ea78586767fe93da65d775
JustAnyone <dom.svetikas@gmail.com>
2021-07-30 16:39:01 +0300

    Add update feature for whatever reason

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         No twitter for you
 // @namespace    https://github.com/JustAnyones/Twitter-remover/
-// @version      0.1
+// @version      0.2
 // @description  Prevents you from opening twitter at any cost.
 // @author       JustAnyone
 // @match        https://twitter.com/*
 // @match        https://mobile.twitter.com/*
 // @icon         https://www.google.com/s2/favicons?domain=twitter.com/
+// @updateURL    https://raw.githubusercontent.com/JustAnyones/Twitter-remover/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/JustAnyones/Twitter-remover/main/script.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
